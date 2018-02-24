3bf618b46eb7b4cc19174430009d1c1fb836d36b
1372290812@qq.com <1372290812@qq.com>
2018-02-24 11:27:45 +0800

    Add logger record to all project use loggermessage

---
+++ LoggerRecord/LoggerExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.Extensions.Logging;
+
+namespace LoggerRecord
+{
+    public class LoggerExtensions
+    {
+        private static readonly Action<ILogger,string,Exception>_loggerRecord;
+        static LoggerExtensions()
+        {
+            _loggerRecord=LoggerMessage.Define<string>(LogLevel.Information,new EventId(1,nameof(LoggerRecord)),
+            "Record logging");
+        }
+
+        public static void LoggerRecord(ILogger logger,string message)
+        {
+            _loggerRecord(logger,message,null);
+        }
+    }
+}
